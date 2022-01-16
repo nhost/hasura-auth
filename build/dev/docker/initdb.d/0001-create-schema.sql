982cc2d881c046c6327b694146c791350e670158
Nuno Pato <nunopato@gmail.com>
2022-01-16 18:45:39 -0100

    Remove storage schema

--- build/dev/docker/initdb.d/0001-create-schema.sql
+++ build/dev/docker/initdb.d/0001-create-schema.sql
@@ -1,15 +1,8 @@
 -- auth schema
 CREATE SCHEMA IF NOT EXISTS auth;
 
--- storage schema
-CREATE SCHEMA IF NOT EXISTS storage;
-
--- public
--- pgcrypt & citext is required to be installed in the
--- `public` schema because of Hasura
 -- https://github.com/hasura/graphql-engine/issues/3657
 CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
-
 CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
 
 CREATE OR REPLACE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
