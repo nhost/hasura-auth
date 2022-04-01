f47579510dc7c6f46d9efb2446299d72d2606b8f
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-04-01 13:14:19 +0200

    feat: insert a new role in `auth.roles` when it is added in the Hasura metadata
    See [this discussion](https://github.com/nhost/nhost/discussions/31)
    
    Closes #103

---
+++ migrations/00005_create-roles-from-metadata.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION auth.update_roles_from_metadata () RETURNS TRIGGER LANGUAGE plpgsql AS $$ BEGIN
+INSERT INTO auth.roles(role)
+SELECT DISTINCT jsonb_array_elements_text(
+        jsonb_path_query_array(
+            new.metadata::jsonb,
+            '$.sources[*].tables[*].select_permissions[*].role'
+        ) || jsonb_path_query_array(
+            new.metadata::jsonb,
+            '$.sources[*].tables[*].insert_permissions[*].role'
+        ) || jsonb_path_query_array(
+            new.metadata::jsonb,
+            '$.sources[*].tables[*].update_permissions[*].role'
+        ) || jsonb_path_query_array(
+            new.metadata::jsonb,
+            '$.sources[*].tables[*].delete_permissions[*].role'
+        )
+    ) as role ON CONFLICT DO NOTHING;
+RETURN new;
+END;
+$$;
+CREATE OR REPLACE TRIGGER update_roles_from_metadata BEFORE
+UPDATE ON hdb_catalog.hdb_metadata FOR EACH ROW EXECUTE FUNCTION auth.update_roles_from_metadata ();
