7426696c0a59ce834e6d0d26c2eb7b34976f6ea9
zbeyens <ziyad.beyens@gmail.com>
2022-04-20 17:15:02 +0200

    fix

--- test/global-setup.ts
+++ test/global-setup.ts
@@ -2,13 +2,11 @@ require('tsconfig-paths/register');
 require('dotenv').config();
 import { Client } from 'pg';
 
-import { applyMetadata } from '@/metadata';
-import { applyMigrations } from '@/migrations';
 import { ENV } from '@/utils';
 
 export default async (): Promise<void> => {
-  await applyMigrations();
-  await applyMetadata();
+  // await applyMigrations();
+  // await applyMetadata();
   const client = new Client({
     connectionString: ENV.HASURA_GRAPHQL_DATABASE_URL,
   });
