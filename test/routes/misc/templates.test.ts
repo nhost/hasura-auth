ddb70f41d26322e76f380a88d7a683e4aa49e0ad
Asen Lekov <asenlekoff@gmail.com>
2022-07-05 19:30:59 +0300

    style: unify names

--- test/routes/misc/templates.test.ts
+++ test/routes/misc/templates.test.ts
@@ -6,7 +6,7 @@ describe('templates', () => {
   it('should render proper sms template', async () => {
     // set env vars
     await request.post('/change-env').send({
-      APP_NAME: 'NHost App',
+      APP_NAME: 'Nhost App',
     });
 
     const template = 'signin-passwordless-sms';
@@ -17,6 +17,6 @@ describe('templates', () => {
       code: '123456',
     });
 
-    expect(message).toEqual('123456 is your verification code for NHost App.');
+    expect(message).toEqual('123456 is your verification code for Nhost App.');
   });
 });
