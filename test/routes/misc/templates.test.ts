88798ebbfc4a70f0e045164dcca81696b9c6d875
Asen Lekov <asenlekoff@gmail.com>
2022-08-22 16:01:07 +0300

    refactor: remove set of APP_NAME env

--- test/routes/misc/templates.test.ts
+++ test/routes/misc/templates.test.ts
@@ -1,13 +1,7 @@
 import { renderTemplate } from '@/templates';
-import { request } from '../../server';
 
 describe('templates', () => {
   it('should render proper sms template', async () => {
-    // set env vars
-    await request.post('/change-env').send({
-      APP_NAME: 'NHost App',
-    });
-
     const template = 'signin-passwordless-sms';
     const message = await renderTemplate(`${template}/text`, {
       locale: 'en',
