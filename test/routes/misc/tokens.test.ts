0f458942a960a370286fc323555702ab3b6f44d2
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-11-29 20:23:33 +0100

    test: refresh token stored either as plain text or sha

--- test/routes/misc/tokens.test.ts
+++ test/routes/misc/tokens.test.ts
@@ -1,9 +1,11 @@
+import { v4 as uuidv4 } from 'uuid';
 import { StatusCodes } from 'http-status-codes';
 import { Client } from 'pg';
+import crypto from 'crypto';
 
 import { ENV } from '../../../src/utils/env';
 import { request } from '../../server';
-import { isValidAccessToken } from '../../utils';
+import { decodeAccessToken, isValidAccessToken } from '../../utils';
 
 describe('token', () => {
   let client: Client;
@@ -13,6 +15,10 @@ describe('token', () => {
       connectionString: ENV.HASURA_GRAPHQL_DATABASE_URL,
     });
     await client.connect();
+    await request.post('/change-env').send({
+      AUTH_DISABLE_NEW_USERS: false,
+      AUTH_ANONYMOUS_USERS_ENABLED: true,
+    });
   });
 
   afterAll(async () => {
@@ -25,10 +31,6 @@ describe('token', () => {
 
   it('should should sign in and get access token with standard claims', async () => {
     // set env vars
-    await request.post('/change-env').send({
-      AUTH_DISABLE_NEW_USERS: false,
-      AUTH_ANONYMOUS_USERS_ENABLED: true,
-    });
 
     const { body } = await request
       .post('/signin/anonymous')
@@ -42,5 +44,53 @@ describe('token', () => {
     expect(typeof accessTokenExpiresIn).toBe('number');
     expect(typeof refreshToken).toBe('string');
     expect(mfa).toBe(null);
+  });
+
+  it('should refresh with a token stored as plain text', async () => {
+    const { body } = await request
+      .post('/signin/anonymous')
+      .send()
+      .expect(StatusCodes.OK);
+
+    const refreshToken = uuidv4();
+    const id = (await decodeAccessToken(body.session.accessToken))?.[
+      'https://hasura.io/jwt/claims'
+    ]['x-hasura-user-id'];
+    await client.query(
+      `INSERT INTO "auth"."refresh_tokens" (refresh_token, user_id, expires_at) VALUES ($1, $2, $3);`,
+      [
+        refreshToken,
+        id,
+        new Date(Date.now() + 1000 * 60 * 60 * 24 * 30).toISOString(),
+      ]
+    );
+
+    await request.post('/token').send({ refreshToken }).expect(StatusCodes.OK);
+  });
+
+  it('should refresh with a token stored as SHA256', async () => {
+    const { body } = await request
+      .post('/signin/anonymous')
+      .send()
+      .expect(StatusCodes.OK);
+
+    const refreshToken = uuidv4();
+    const hashedToken = crypto
+      .createHash('sha256')
+      .update(refreshToken)
+      .digest('hex');
+    const id = (await decodeAccessToken(body.session.accessToken))?.[
+      'https://hasura.io/jwt/claims'
+    ]['x-hasura-user-id'];
+    await client.query(
+      `INSERT INTO "auth"."refresh_tokens" (refresh_token, user_id, expires_at) VALUES ($1, $2, $3);`,
+      [
+        hashedToken,
+        id,
+        new Date(Date.now() + 1000 * 60 * 60 * 24 * 30).toISOString(),
+      ]
+    );
+
+    await request.post('/token').send({ refreshToken }).expect(StatusCodes.OK);
   });
 });
