fb192f8a149a8985f677165c9b340c2aa08d7bb2
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2022-12-13 11:50:00 +0100

    chore: correct sql comment

--- migrations/00011_hashed_refresh_tokens.sql
+++ migrations/00011_hashed_refresh_tokens.sql
@@ -8,7 +8,7 @@ CREATE TABLE "auth"."hashed_refresh_tokens" (
 ALTER TABLE auth.hashed_refresh_tokens
     ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES auth.users (id) ON UPDATE CASCADE ON DELETE CASCADE;
 
-COMMENT ON TABLE auth.refresh_tokens IS 'Hashed user refresh tokens. Hasura auth uses them to rotate new access tokens as long as the refresh token is not expired. Don''t modify its structure as Hasura Auth relies on it to function properly.';
+COMMENT ON TABLE auth.hashed_refresh_tokens IS 'Hashed user refresh tokens. Hasura auth uses them to rotate new access tokens as long as the refresh token is not expired. Don''t modify its structure as Hasura Auth relies on it to function properly.';
 
-COMMENT ON TABLE auth.hashed_refresh_tokens IS 'Deprecated: user refresh tokens. This table is replaced by hashed_refresh_tokens and will be removed in the next major release.';
+COMMENT ON TABLE auth.refresh_tokens IS 'Deprecated: user refresh tokens. This table is replaced by hashed_refresh_tokens and will be removed in the next major release.';
 
