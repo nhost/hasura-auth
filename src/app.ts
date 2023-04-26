3ef9b1536b7905981ef84713b3c1ab39b9c1aa0b
Szilárd Dóró <doroszilard@gmail.com>
2023-04-26 12:22:07 +0200

    chore: improve log verbosity

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-import express from 'express';
-import helmet from 'helmet';
 import { json } from 'body-parser';
 import cors from 'cors';
-
-import router from './routes';
+import express from 'express';
+import helmet from 'helmet';
 import { serverErrors } from './errors';
+import { httpLogger, uncaughtErrorLogger } from './logger';
 import { authMiddleware } from './middleware/auth';
 import { addOpenApiRoute } from './openapi';
-import { uncaughtErrorLogger, httpLogger } from './logger';
+import router from './routes';
 
 const app = express();
 
@@ -16,12 +15,11 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 addOpenApiRoute(app);
-app.use(httpLogger);
 
+app.use(httpLogger);
 app.use(helmet(), json(), cors());
 app.use(authMiddleware);
-
 app.use(router);
-
 app.use(uncaughtErrorLogger, serverErrors);
+
 export { app };
