deab73a8653aca1033113e9a71e3d6e31e7ba8a7
Szilárd Dóró <doroszilard@gmail.com>
2023-05-03 14:06:42 +0200

    revert image

--- migrations/00014_rename_refresh_token_column.sql
+++ migrations/00014_rename_refresh_token_column.sql
@@ -3,5 +3,8 @@ ALTER TABLE auth.refresh_tokens
 RENAME COLUMN refresh_token TO id;
 COMMENT ON COLUMN auth.refresh_tokens.id IS NULL;
 ALTER TABLE auth.refresh_tokens ALTER COLUMN id SET DEFAULT gen_random_uuid();
-ALTER TABLE auth.refresh_tokens ALTER COLUMN refresh_token_hash DROP EXPRESSION IF EXISTS;
+ALTER TABLE auth.refresh_tokens RENAME COLUMN refresh_token_hash TO refresh_token_hash_old;
+ALTER TABLE auth.refresh_tokens ADD COLUMN refresh_token_hash TEXT NOT NULL;
+UPDATE auth.refresh_tokens SET refresh_token_hash = refresh_token_hash_old;
+ALTER TABLE auth.refresh_tokens DROP COLUMN refresh_token_hash_old;
 COMMIT;
