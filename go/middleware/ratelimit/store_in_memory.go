42940770ba7fb79f1eb691fd612e4571c0830e92
David Barroso <dbarrosop@dravetech.com>
2024-08-17 09:33:27 +0200

    asd

--- go/middleware/ratelimit/store_in_memory.go
+++ go/middleware/ratelimit/store_in_memory.go
@@ -12,20 +12,17 @@ type InMemoryStoreValue struct {
 
 type InMemoryStore struct {
 	data map[string]InMemoryStoreValue
-	mx   sync.RWMutex
+	mx   sync.Mutex
 }
 
 func NewInMemoryStore() *InMemoryStore {
 	return &InMemoryStore{
 		data: make(map[string]InMemoryStoreValue),
-		mx:   sync.RWMutex{},
+		mx:   sync.Mutex{},
 	}
 }
 
-func (i *InMemoryStore) DeleteExpired() {
-	i.mx.Lock()
-	defer i.mx.Unlock()
-
+func (i *InMemoryStore) deleteExpired() {
 	for k, v := range i.data {
 		if time.Now().After(v.time) {
 			delete(i.data, k)
@@ -33,25 +30,29 @@ func (i *InMemoryStore) DeleteExpired() {
 	}
 }
 
-func (i *InMemoryStore) Get(key string) int {
-	i.DeleteExpired()
-
-	i.mx.RLock()
-	defer i.mx.RUnlock()
-
+func (i *InMemoryStore) get(key string) int {
 	if v, ok := i.data[key]; ok {
 		return v.v
 	}
 	return 0
 }
 
-func (i *InMemoryStore) Increment(key string, expire time.Duration) int {
-	i.DeleteExpired()
-
+func (i *InMemoryStore) Get(key string) int {
 	i.mx.Lock()
 	defer i.mx.Unlock()
 
-	current := i.Get(key)
+	i.deleteExpired()
+
+	return i.get(key)
+}
+
+func (i *InMemoryStore) Increment(key string, expire time.Duration) int {
+	i.mx.Lock()
+	defer i.mx.Unlock()
+
+	i.deleteExpired()
+
+	current := i.get(key)
 	i.data[key] = InMemoryStoreValue{
 		v:    current + 1,
 		time: time.Now().Add(expire),
